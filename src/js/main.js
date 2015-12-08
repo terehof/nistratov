1287164dd50605fe51d4b85859fe24d721da0916
terehov <terehovsergej@gmail.com>
2015-12-09 00:54:13 +0200

    main slicing ended. responsive left

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ app.events = {
     init: function() {
         this.tile();
         this.sliderHome();
+        this.map();
     },
     tile: function () {
         var $tile = $('.tile');
@@ -20,6 +21,19 @@ app.events = {
                 nextText: '',
                 controlsContainer: $('.custom-controls-container'),
                 customDirectionNav: $('.custom-navigation a')
+            });
+        }
+    },
+    map: function () {
+        var $map = $('#map');
+        if ($map.length > 0) {
+            var map;
+            DG.then(function () {
+                map = DG.map('map', {
+                    center: [43.15, 131.91],
+                    zoom: 18
+                });
+                DG.marker([43.15, 131.91]).addTo(map);
             });
         }
     }
